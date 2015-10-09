63fe9ed049e9f650910caac324689f137a8adda7
Thornton Prime <thornton.prime@gmail.com>
2015-10-08 17:21:56 -0700

    Basic file server functionality

---
+++ auth/auth.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"io"
+	"fmt"
+	"net/http"
+)
+
+type AuthHandler struct {
+        handler http.Handler
+        out     io.Writer
+}
+
+func (h *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+
+	if cs := r.TLS; len(cs.PeerCertificates) != 0 {
+		for _, crt := range cs.PeerCertificates {
+			fmt.Fprintf(h.out, "Common name: %v\n", crt.Subject.CommonName)
+			fmt.Fprintf(h.out, "Subject Names: %v\n", crt.Subject.Names)
+			fmt.Fprintf(h.out, "Extra Names: %v\n", crt.Subject.ExtraNames)
+			fmt.Fprintf(h.out, "Signature: %X\n", crt.Signature)
+		}
+	}
+
+	h.handler.ServeHTTP(w, r)
+
+}
+
+
+func NewAuthHandler(handler http.Handler, out io.Writer) http.Handler {
+	return &AuthHandler{
+		handler: handler,
+		out:     out,
+	}
+}
+
---
+++ route/handler.go
@@ -0,0 +1,25 @@
+package route
+
+import (
+	"net/http"
+)
+
+
+type HoardHandler struct {
+	http.Handler
+}
+
+func (_ *HoardHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+
+	switch r.Method {
+
+	default:
+		http.Error(w, "Method not allowed", 405)
+
+	case "GET", "HEAD":
+		http.FileServer(http.Dir("./data")).ServeHTTP(w, r)
+
+	}
+}
+
